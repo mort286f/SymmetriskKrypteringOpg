e107aa25423734f67db2010b4357f91bef080645
mortenvestergaard <57791299+mortenvestergaard@users.noreply.github.com>
2021-01-11 13:51:46 +0100

    Add files via upload

---
+++ SymmetriskKrypteringOpg/SymmetriskKrypteringOpg/Encryption.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetriskKrypteringOpg
+{
+    class Encryption
+    {
+        SymmetricAlgorithm symmetricAlg;
+        public void GenerateSymmetricAlg(int number)
+        {
+            switch (number)
+            {
+                //DES
+                case 1:
+                    symmetricAlg = DES.Create();
+                    break;
+                //TripleDES
+                case 2:
+                    symmetricAlg = TripleDES.Create();
+                    break;
+                //Rijndael
+                case 3:
+                    symmetricAlg = Rijndael.Create();
+                    break;
+                default:
+                    break;
+            }
+            symmetricAlg.GenerateKey();
+            symmetricAlg.GenerateIV();
+        }
+
+        public byte[] GenerateRandomNumber(int count)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var numbers = new byte[count];
+                randomNumberGenerator.GetBytes(numbers);
+
+                return numbers;
+            }
+        }
+
+        public byte[] Encrypt(byte[] message)
+        {
+            symmetricAlg.Mode = CipherMode.CBC;
+            symmetricAlg.Padding = PaddingMode.PKCS7;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, symmetricAlg.CreateEncryptor(),
+                    CryptoStreamMode.Write);
+
+                cryptoStream.Write(message, 0, message.Length);
+                cryptoStream.FlushFinalBlock();
+
+                return memoryStream.ToArray();
+            }
+        }
+
+        public byte[] Decrypt(byte[] encryptedMsg)
+        {
+            symmetricAlg.Mode = CipherMode.CBC;
+            symmetricAlg.Padding = PaddingMode.PKCS7;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, symmetricAlg.CreateDecryptor(),
+                    CryptoStreamMode.Write);
+
+                cryptoStream.Write(encryptedMsg, 0, encryptedMsg.Length);
+                cryptoStream.FlushFinalBlock();
+
+                return memoryStream.ToArray();
+            }
+        }
+
+    }
+}
---
+++ SymmetriskKrypteringOpg/SymmetriskKrypteringOpg/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetriskKrypteringOpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encryption encryption = new Encryption();
+            Console.WriteLine("Please choose your way of encrypting:");
+            Console.WriteLine("(1) DES");
+            Console.WriteLine("(2) TripleDES");
+            Console.WriteLine("(3) Rijndael");
+            int chosenEncr = int.Parse(Console.ReadLine());
+            encryption.GenerateSymmetricAlg(chosenEncr);
+            Console.WriteLine("Please write your message:");
+            string message = Console.ReadLine();
+            
+            switch (chosenEncr)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("Encryption with DES");
+                    Console.WriteLine("Message in plaintext: " + message);
+                    byte[] encryptedDESMsg = encryption.Encrypt(Encoding.ASCII.GetBytes(message));
+                    Console.WriteLine("Encrypted message: " + Convert.ToBase64String(encryptedDESMsg));
+                    Console.WriteLine("Decrypted message: " + Convert.ToBase64String(encryption.Decrypt(encryptedDESMsg)));
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("Encryption with TripleDES");
+                    byte[] encryptedTripleDESMsg = encryption.Encrypt(Encoding.ASCII.GetBytes(message));
+                    Console.WriteLine("Encrypted message: " + Convert.ToBase64String(encryptedTripleDESMsg));
+                    Console.WriteLine("Decrypted message: " + Convert.ToBase64String(encryption.Decrypt(encryptedTripleDESMsg)));
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("Encryption with Rijndael");
+                    byte[] encryptedRijndaelMsg = encryption.Encrypt(Encoding.ASCII.GetBytes(message));
+                    Console.WriteLine("Encrypted message: " + Convert.ToBase64String(encryptedRijndaelMsg));
+                    Console.WriteLine("Decrypted message: " + Convert.ToBase64String(encryption.Decrypt(encryptedRijndaelMsg)));
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
